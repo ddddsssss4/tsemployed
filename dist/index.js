4ce0099e0a20485a257687e6deaa12fa7b93c3f7
aryansingh98875 <aryansingh98875@gmail.com>
2024-10-13 01:13:18 +0530

    added db

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,19 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const db_1 = __importDefault(require("./db"));
 // Create an Express app
 const app = (0, express_1.default)();
 // Middleware to parse JSON requests
@@ -13,9 +23,20 @@ app.get('/', (req, res) => {
     res.send('Hello, TypeScript with Express!');
 });
 // Another route
-app.get('/api/greet', (req, res) => {
-    res.json({ message: 'Hello from API!' });
+app.get('/api/greet', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const users = yield db_1.default.user.findMany();
+    res.json({ users });
+}));
+app.get('/api/users', (req, res) => {
+    res.json({ message: 'Hello from users!' });
 });
+app.post('/api/checkdb', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { name } = req.body;
+    const user = yield db_1.default.user.create({
+        data: { name }
+    });
+    res.json(user);
+}));
 // Start the server
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
--- index.ts
+++ index.ts
@@ -1,7 +1,9 @@
 import express, { Request, Response } from 'express';
+import db from './db';
 
 // Create an Express app
 const app = express();
+
 
 // Middleware to parse JSON requests
 app.use(express.json());
@@ -12,9 +14,29 @@ app.get('/', (req: Request, res: Response) => {
 });
 
 // Another route
-app.get('/api/greet', (req: Request, res: Response) => {
-  res.json({ message: 'Hello from API!' });
+app.get('/api/greet', async (req: Request, res: Response) => {
+  const users= await db.user.findMany();
+  res.json({ users });
 });
+
+app.get('/api/users', (req: Request, res: Response) => {
+  res.json({ message: 'Hello from users!' });
+});
+
+
+app.post('/api/checkdb',async (req,res)=>{
+  const {name} =req.body;
+
+
+  const user = await db.user.create({
+    data: { name }
+  });
+
+
+  res.json(user);
+})
+
+
 
 // Start the server
 const PORT = process.env.PORT || 8080;
